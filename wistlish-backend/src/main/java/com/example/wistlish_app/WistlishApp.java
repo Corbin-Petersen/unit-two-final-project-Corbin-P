fc005366b96a4bb69cb3a37b7655c44e2ad99402
Corbin-Petersen <corbincpetersen@gmail.com>
2025-07-14 16:46:09 -0500

    initialized spring boot backend

---
+++ wistlish-backend/src/main/java/com/example/wistlish_app/WistlishApp.java
@@ -0,0 +1,13 @@
+package com.example.wistlish_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WistlishApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WistlishApp.class, args);
+	}
+
+}
---
+++ wistlish-backend/src/test/java/com/example/wistlish_app/WistlishAppTests.java
@@ -0,0 +1,13 @@
+package com.example.wistlish_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WistlishAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
